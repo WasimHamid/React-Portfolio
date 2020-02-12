a9064886557a9b9370ec19c0e7032c5da79313cc
unknown <wasim.hamid45@gmail.com>
2020-02-12 20:12:21 +0000

    seperate componants

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./css/styles.css"
+
+class Contact extends React.Component {
+    render () {
+  return (
+    <>
+        <section id="Contact">
+            <div className="Training-text">
+                <h2>Samantha Orton</h2>
+                <p>"My daughter is definitely stronger, more confident and fitter since joining Playermade, but most off all she enjoys going and always has a smile on her face after every session." (Blaby & Whetstone and Leicester Ladies - Football)</p>
+                <h2>Julie Smith</h2>
+                <p>"I have nothing but high praise for Playermade, our daughter has been attending their classNamees since the summer 2018 and has made new friends, grown in strength, speed and her balance has also improved. England have also noticed a big difference
+                    in her strength and movement since she has been attending."(Leicester Ladies and England - Football)</p>
+            </div>
+        </section>
+    </>
+    );
+ }
+}
+
+export default Contact;
