463c8f025a8c026eb186b33e4a979472c3b07783
unknown <wasim.hamid45@gmail.com>
2020-02-12 21:01:10 +0000

    sorting folders and home screen

---
+++ src/components/Home/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+// import Pdf from "../Documents/WasimHamidCV.pdf";
+import css from "./Home.module.css";
+
+
+class Home extends React.Component {
+    render () {
+  return (
+    <>
+        <section id="home" data-module-id="screen">
+        <div className={css.bannertext}>
+        <h2>Junior Developer</h2>
+
+        <p>React | JavaScript | HTML | CSS | Express | MongoDB | React Native</p>
+
+        <div>
+          <a
+            className={css.button1}
+            href={Pdf}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download CV
+          </a>
+        </div>
+      </div>
+        </section>
+    </>
+    );
+ }
+}
+
+export default Home;
