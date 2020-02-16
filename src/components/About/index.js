f07a3687317a8ff395701f520abbb30e85404a65
unknown <wasim.hamid45@gmail.com>
2020-02-16 22:20:29 +0000

    moved css file

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../css/styles.css"
+import "../../../public/css/styles.css"
 
 class About extends React.Component {
     render () {
@@ -8,21 +8,19 @@ class About extends React.Component {
         <section id="About-Me">
         <div className="Member-text">
                 <div className="border-box">
-                    <h1>SCREEN</h1>
+                    <h1>COLLEGE</h1>
                     <hr />
                     <p>Identify your individual training needs, goals and pathway for future training through our in-house sports related strength and movement screen</p>
-                    <div className="vl"></div>
                 </div>
 
                 <div className="border-box">
-                    <h1>TRAIN</h1>
+                    <h1>BOOTCAMP</h1>
                     <hr/>
                     <p>Chose the most suited and convenient means of training, whether that be through our weekly small group training system or bespoke 1:1 coaching</p>
-                    <div className="vl"></div>
                 </div>
 
                 <div className="border-box">
-                    <h1>GAME</h1>
+                    <h1>FREELANCE</h1>
                     <hr/>
                     <p>Take all that you have worked on into your game, showing higher levels of performance week in week out</p>
                 </div>
