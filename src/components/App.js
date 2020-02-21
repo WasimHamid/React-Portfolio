4435704ed8db0f013be57835f60acb8881bbf006
WasimHamid <44105571+WasimHamid@users.noreply.github.com>
2020-02-21 21:24:24 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { stack as Menu } from 'react-burger-menu'
 
 import Home from "../components/Home"
 import Project from "../components/Project"
@@ -7,20 +8,31 @@ import Hobbies from "../components/Hobbies"
 import Contact from "../components/Contact"
 
 class App extends React.Component {
+  showSettings (event) {
+
+    event.preventDefault();
+
+    .
+
+    .
+
+    .
+
+  }
+
     render () {
   return (
     <>
     <header>
         <a href="#home" className="logo" data-scroll>Wasim Hamid</a>
-        <nav className="nav-collapse">
-            <ul>
-                <li className="menu-item active"><a href="#home" data-scroll>Home</a></li>
-                <li className="menu-item"><a href="#Project-Page" data-scroll>Project Page</a></li>
-                <li className="menu-item"><a href="#About-Me" data-scroll>About Me</a></li>
-                <li className="menu-item"><a href="#Hobbies-Page" data-scroll>Hobbies Page</a></li>
-                <li className="menu-item"><a href="#Contact" data-scroll>Contact Us</a></li>
-            </ul>
-        </nav>
+        <Menu>
+            <a href="#home" data-scroll>Home</a>
+            <a href="#Project-Page" data-scroll>Project Page</a>
+            <a href="#About-Me" data-scroll>About Me</a>
+            <a href="#Hobbies-Page" data-scroll>Hobbies Page</a>
+            <a href="#Contact" data-scroll>Contact Us</a>
+            <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
     </header>
     <main>
 
