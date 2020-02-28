4b4b7a7e73ee79985597159ae714f5bee9207beb
WasimHamid <44105571+WasimHamid@users.noreply.github.com>
2020-02-28 19:27:05 +0000

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,7 +7,7 @@ class Contact extends React.Component {
         <section id="Contact">
              <div class="contact-text">
                 <a
-                    href="http://linkedin.com"
+                    href="http://linkedin.com/in/wasim-hamid-a4858b185"
                     rel="noopener noreferrer"
                     target="_blank"
                 >
@@ -21,14 +21,14 @@ class Contact extends React.Component {
                     <i class="fa fa-github-square" aria-hidden="true"></i>
                 </a>
                 <a
-                    href="https://github.com/WasimHamid"
+                    href="mailto: wasim.hamid45@gmail.com"
                     rel="noopener noreferrer"
                     target="_blank"
                 >
                     <i class="fa fa-envelope-square" aria-hidden="true"></i>
                 </a>
                 <a
-                    href="https://github.com/WasimHamid"
+                    href="tel:07599001781"
                     rel="noopener noreferrer"
                     target="_blank"
                 >
