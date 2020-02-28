fcfba6f2d95437edb4a3deae958d2e4257cae66b
WasimHamid <44105571+WasimHamid@users.noreply.github.com>
2020-02-28 14:17:19 +0000

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,6 +8,7 @@ class Contact extends React.Component {
              <div class="contact-text">
                 <p>Email: Wasim.hamid45@gmail.com</p>
                 <p>Github: https://github.com/WasimHamid</p>
+                <p>LinkedIn: https://www.linkedin.com/in/wasim-hamid-a4858b185</p>
                 <p>Mobile: 07599 001781</p>
             </div>
         </section>
