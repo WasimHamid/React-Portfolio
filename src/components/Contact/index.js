7704916537e5f4db099d1e8dec200c559a536c77
WasimHamid <44105571+WasimHamid@users.noreply.github.com>
2020-02-28 19:01:58 +0000

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,10 +6,10 @@ class Contact extends React.Component {
     <>
         <section id="Contact">
              <div class="contact-text">
-                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-                <i class="fa fa-github-square" aria-hidden="true"></i>
-                <i class="fa fa-envelope-square" aria-hidden="true"></i>
-                <i class="fa fa-phone-square" aria-hidden="true"></i>
+                <i class="fa fa-linkedin-square" style="font-size:24px" aria-hidden="true"></i>
+                <i class="fa fa-github-square" style="font-size:24px" aria-hidden="true"></i>
+                <i class="fa fa-envelope-square" style="font-size:24px" aria-hidden="true"></i>
+                <i class="fa fa-phone-square" style="font-size:24px" aria-hidden="true"></i>
             </div>
         </section>
     </>
