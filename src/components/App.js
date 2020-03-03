e6e89d8916fe6515d5a6f1aba11b97d20bf2d43f
WasimHamid <44105571+WasimHamid@users.noreply.github.com>
2020-03-03 15:33:44 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Navigation from "../Nav"
+import Navigation from "../components/Nav"
 import Home from "../components/Home"
 import Project from "../components/Project"
 import About from "../components/About"
