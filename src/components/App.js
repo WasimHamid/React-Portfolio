6ce36b2906c3a0b84991dac2d0240e4d64f92346
unknown <wasim.hamid45@gmail.com>
2020-03-03 15:16:54 +0000

    test

--- src/components/App.js
+++ src/components/App.js
@@ -13,32 +13,31 @@ const App = () => {
      const toggle = () => setIsOpen(!isOpen)
   return (
     <>
-    <Navbar color="faded" light>
-        <NavbarBrand href="#home" class="logo" data-scroll>Wasim Hamid</NavbarBrand>
-        <NavbarToggler onClick={toggle} className="mr-2"/>
-        <Collapse isOpen={isOpen} navbar>
-            <Nav navbar>
-                <NavItem>
-                    <NavLink href="#home">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#Project-Page">Project</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#About-Me">About</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#Hobbies-Page">Hobbies</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#Contact">Contact</NavLink>
-                </NavItem>
-            </Nav>
-        </Collapse>   
-    </Navbar>
     <main>
-
-        
+        <Navbar color="faded" light>
+            <NavbarBrand href="#home" class="logo" data-scroll>Wasim Hamid</NavbarBrand>
+            <NavbarToggler onClick={toggle} className="mr-2"/>
+            <Collapse isOpen={isOpen} navbar>
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink href="#home">Home</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#Project-Page">Project</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#About-Me">About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#Hobbies-Page">Hobbies</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#Contact">Contact</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>   
+        </Navbar>
+  
         <Home />
         <Project />
         <About />
